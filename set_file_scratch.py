de874b6db0930a247355dc8e903a382a0c513477
Andras Verbenyi <headhunter.germany@gmail.com>
2025-06-11 17:27:48 +0200

    book stats function and list of files

--- set_file_scratch.py
+++ set_file_scratch.py
@@ -11,13 +11,20 @@ def get_unique_words(text_str):
     split_words = text_str.split()
     return split_words
 
-
-def main():
-    filename = 'frankenstein.txt'
+def book_stats(filename):
+    print(filename)
+    print('++++++++++++++++++++++++++++')
     textfile_plain = ''
     textfile_plain = read_file(filename)
     word_list = []
-    word_list = get_unique_words(textfile_plain)
-    print(word_list[89])
+    word_list = get_unique_words(textfile_plain.lower())
+    # print(word_list[89])
+    uniques = set(word_list)
+    print(f'Der Text hat insgesamt {len(word_list)} Woerter und {len(uniques)} unterschiedliche Woerter')
+
+def main():
+    filename = ['books/3.txt','books/4.txt','books/5.txt']
+    for f in filename:
+        book_stats(f)
 
 main()
