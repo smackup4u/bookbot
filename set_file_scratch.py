0f2409cdf2cdf45f0923a7b857e6deea80f7bc5d
Andras Verbenyi <headhunter.germany@gmail.com>
2025-06-11 15:31:20 +0200

    first iteration text file scratch

--- set_file_scratch.py
+++ set_file_scratch.py
@@ -1,15 +1,23 @@
-#
-//  set_file_scratch.py
-//  
-//
-//  Created by Andras Verbenyi on 6/11/25.
-//
+
 
 def read_file(path):
-    
+    text_string = ''
+    with open(path) as f:
+        text_string = f.read()
+    return text_string
 
-def main()
+def get_unique_words(text_str):
+    split_words = []
+    split_words = text_str.split()
+    return split_words
 
 
+def main():
+    filename = 'frankenstein.txt'
+    textfile_plain = ''
+    textfile_plain = read_file(filename)
+    word_list = []
+    word_list = get_unique_words(textfile_plain)
+    print(word_list[89])
 
 main()
